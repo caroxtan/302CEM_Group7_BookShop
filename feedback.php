5e3f4c1fb7219a476897b55584a22e575006593e
Chia Ying <66930760+Chiayingcy@users.noreply.github.com>
2022-06-12 15:16:14 +0800

    Update version feedback
    some changes code in feedback

--- feedback.php
+++ feedback.php
@@ -21,15 +21,10 @@
 		<h1 align="center">Feedback Form</h1>
                 
         <br/><br/>
+       
         <div>
-            <label for="fname">First Name: </label>
-            <input type="text" id="fname" name="fname" placeholder="Enter your First Name">
-        </div>
-        
-        <br/><br/>
-        <div>
-            <label for="lname">Last Name: </label>
-            <input type="text" id="lname" name="lname" placeholder="Enter your Last Name">
+            <label for="name">Name: </label>
+            <input type="text" id="name" name="name" placeholder="Enter your Name">
         </div>
                 
         <br/><br/>
@@ -78,32 +73,22 @@
     include("bookshop_database.php");
         if (isset($_POST['submitted'])) {
 
-                $fname = $_POST['fname'];
-                $lname = $_POST['lname'];
+                $name = $_POST['name'];
                 $email = $_POST['email'];
                 $service = $_POST['service'];
                 $suggestion = $_POST['suggestion'];
 
-                $fname = mysqli_real_escape_string($combine, $fname);
-                $lname = mysqli_real_escape_string($combine, $lname);
+                $name = mysqli_real_escape_string($combine, $name);
                 $email = mysqli_real_escape_string($combine, $email);
                 $service = mysqli_real_escape_string($combine, $service);
                 $suggestion = mysqli_real_escape_string($combine, $suggestion);
 
-                if(empty($fname)) {
-                    echo "<script>alert('First name required!')</script>";
+                if(empty($name)) {
+                    echo "<script>alert('Name is required!')</script>";
                 }
 
-                else if(!(preg_match('/^[a-zA-Z\s]+$/', $fname))) {
-                    echo "<script>alert('First name must be in alphabets!')</script>";
-                }
-
-                else if(empty($lname)) {
-                    echo "<script>alert('Last name required!')</script>";
-                }
-
-                else if(!(preg_match('/^[a-zA-Z\s]+$/', $lname))) {
-                    echo "<script>alert('Last name must be in alphabets!')</script>";
+                else if(!(preg_match('/^[a-zA-Z\s]+$/', $name))) {
+                    echo "<script>alert('Name must be in alphabets!')</script>";
                 }
 
                 else if(empty($email)) {
@@ -121,8 +106,8 @@
                 else{
                     //success combine data and display message
                     $query = mysqli_query($combine, "INSERT INTO feedback
-                        (fname, lname, email, service, suggestion ) VALUES
-                        ('$fname', '$lname', '$email ', '$service', '$suggestion')");
+                        (name, email, service, suggestion ) VALUES
+                        ('$name', '$email ', '$service', '$suggestion')");
                         if ($query)
                         {
                             echo "<script>alert('Thanks for your feedback!');
