e12ce3ccd794cf37108a2a1113fdba5d57944663
caroxtan <66262347+caroxtan@users.noreply.github.com>
2022-07-03 00:33:03 +0800

    Fixed increase stock function bug
    -Placed the calculation before the SQL command
    -Removed ' ' from $new_stock variable as it is a number
    Admin is able to increase book stock

--- addon_stock.php
+++ addon_stock.php
@@ -61,7 +61,7 @@
 	echo"</div>";
 	echo"<div class='main'>";
 	
-	echo"<h1 align='center'>Add-on Stock</h1>";
+	echo"<h1 align='center'>Edit Stock</h1>";
 	
 	$current_date = date("Y-m-d");
 
@@ -72,31 +72,40 @@
 	
 	echo "<center>";
     echo"<label>Book Name:</label>";
-    echo"<br /><input type='text' id='book_name' name='book_name' placeholder='Book Name' value='".$row['book_name']."'; size='50' readonly>";
+    echo"<br /><input type='text' id='book_name' name='book_name' placeholder='Book Name' value='".$row['book_name']."'; size='50'>";
 	
 	echo"<br /><br /><label>Book Author:</label>";
-    echo"<br /><input type='text' id='book_author' name='book_author' placeholder='Book Author' value='".$row['book_author']."' size='50' readonly>";
+    echo"<br /><input type='text' id='book_author' name='book_author' placeholder='Book Author' value='".$row['book_author']."' size='50'>";
 	
 	echo"<br /><br /><label>Publication Date:</label>";
-    echo"<br /><input type='date' id='book_date' name='book_date' placeholder='Publication Date' max='$current_date' value='".$row['book_date']."' size='50' readonly>";
+    echo"<br /><input type='date' id='book_date' name='book_date' placeholder='Publication Date' max='$current_date' value='".$row['book_date']."' size='50'>";
 	
 	echo"<br /><br /><label>ISBN-13 Number:</label>";
     echo"<br /><input type='text' id='book_isbn13' name='book_isbn13' placeholder='ISBN-13 Number' value='".$row['book_isbn13']."'  size='50' readonly>";
 	
 	echo"<br /><br /><label>Book Description:</label>";
-	echo"<br /><textarea rows = '5' cols = '48' id='book_description' name='book_description' placeholder='Book Description' readonly>".$row['book_description']."</textarea>";
+	echo"<br /><textarea rows = '5' cols = '48' id='book_description' name='book_description' placeholder='Book Description'>".$row['book_description']."</textarea>";
 	
 	echo"<br /><br /><label>Book Category:</label>";
-    echo"<br /><input type='text' id='book_category' name='book_category' placeholder='Book Category' value='".$row['book_category']."' size='50' readonly>";
+    echo"<br /><input type='text' id='book_category' name='book_category' placeholder='Book Category' value='".$row['book_category']."' size='50'>";
+	
+	echo"<br /><br /><label>Trade Price:</label>";
+	echo"<br /><input type='range' min='1' max='100' value='".$row['book_trade_price']."' name='book_trade_price' id='book_trade_price' onchange='showRangeValueTrade(this.value)' >";
+	echo"<input type='text' id='trade_price' value='".$row['book_trade_price']."' readonly>";
+	
+	echo"<br /><br /><label>Retail Price:</label>";
+	echo"<br /><input type='range' min='1' max='100' value='".$row['book_retail_price']."' name='book_retail_price' id='book_retail_price' onchange='showRangeValueRetail(this.value)' >";
+	echo"<input type='text' id='retail_price' value='".$row['book_retail_price']."' readonly>";
 	
 	echo"<br /><br /><label>Quantity:</label>";
+	echo"<br /><input type='range' min='1' max='20' value='".$row['book_quantity']."' name='book_quantity' id='book_quantity' onchange='showRangeValueQuantity(this.value)' >";
 	echo"<input type='text' id='quantity' value='".$row['book_quantity']."' readonly>";
 	
 	echo"<br /><br /><label>Add-on Quantity:</label>";
-	echo"<br /><input type='range' min='1' max='20' value='1' name='addon_book_quantity' id='addon_book_quantity' onchange='showRangeValueAddonQuantity(this.value)' >";
-	echo"<input type='text' id='addon_quantity' value='1' readonly>";
+	echo"<br /><input type='range' min='1' max='20' value='1' name='book_addon' id='book_addon' onchange='showRangeValueAddOn(this.value)' >";
+	echo"<input type='text' id='add_on' value='1' readonly>";
 	
-	echo "<br /><br /><input type='submit' name='submit' value='Submit'>";
+	echo "<br /><br /><input type='submit' name='submit' value='Submit' onClick=\"javascript: return confirm('Are you sure you want to update?');\">";
 	echo"</form></div></div>";
 	echo"</center>";
 	
@@ -119,7 +128,10 @@
 			document.getElementById('quantity').value=val;
 		}
 		
-		function showRangeValueAddonQuantity(val){
-			document.getElementById('addon_quantity').value=val;
+		function showRangeValueAddOn(val){
+			document.getElementById('add_on').value=val;
 		}
 	</script>
+	
+
+
--- addon_stock2.php
+++ addon_stock2.php
@@ -14,16 +14,18 @@
 	
 	$book_isbn13 = $_GET['book_isbn13'];
 	$book_quantity = $_GET['book_quantity'];
-	$addon_book_quantity = $_GET['addon_book_quantity'];
+	$book_addon = $_GET['book_addon'];
 	
-	$new_book_quantity=$book_quantity+$addon_book_quantity;
+	$new_stock = $book_quantity + $book_addon;
 	
 	$query = "SELECT * FROM book WHERE book_isbn13 = '$book_isbn13'";
+
 	$result = mysqli_query($combine, $sql);
 	$row= mysqli_fetch_array($result, MYSQLI_ASSOC);
 	
+	
 	//edit user profile 
-	$sqlEditing="UPDATE `book` SET `book_quantity`='$new_book_quantity' WHERE `book`.`book_isbn13`='$book_isbn13'";
+	$sqlEditing="UPDATE `book` SET `book_quantity`= $new_stock WHERE `book`.`book_isbn13`='$book_isbn13'";
 		
 		//successful edited
 		if($combine->query($sqlEditing)===TRUE){
